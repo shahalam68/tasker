7801a59ebb94567cee29e3fe1ef7ff5217540f74
shahalam68 <a.k.mshahalam68.com>
2024-02-07 00:17:39 +0600

    markup created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import TaskBoard from "./components/Task/TaskBoard";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <TaskBoard />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/Task/TaskBoard.jsx
@@ -0,0 +1,22 @@
+import SearchTask from "./SearchTask";
+import TaskActions from "./TasKActions";
+import TaskList from "./TaskList";
+
+export default function TaskBoard() {
+  return (
+    <section className="mb-20" id="tasks">
+      <div className="container">
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <div className="mb-14 items-center justify-between sm:flex">
+            <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
+            <div className="flex items-center space-x-5">
+              <SearchTask></SearchTask>
+              <TaskActions></TaskActions>
+            </div>
+          </div>
+          <TaskList></TaskList>
+        </div>
+      </div>
+    </section>
+  );
+}
